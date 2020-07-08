23aa118e0a255d08cfd98f93cd04566f4ac1f5cf
haderman <hadermania@gmail.com>
2020-07-07 19:02:15 -0500

    Add svg support and svg icons

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="border-bottom-s border-color-strong">
+    <header className="border-bottom-s border-color-strong flex">
       <h3>COVID-19 Tracker</h3>
     </header>
   )
