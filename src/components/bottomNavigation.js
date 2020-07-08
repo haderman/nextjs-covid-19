d144ff6dcdd8aa0d2156170df19510bdf01bfa42
haderman <hadermania@gmail.com>
2020-07-07 20:16:52 -0500

    Add icons to bottom navigation

--- src/components/bottomNavigation.js
+++ src/components/bottomNavigation.js
@@ -2,30 +2,38 @@ import Link from "next/link"
 import { useRouter } from 'next/router'
 import classnames from 'classnames'
 
+import WorldwideIcon from "../svg/worldwide.svg"
+import ListIcon from "../svg/list-1.svg"
+import InfoIcon from "../svg/info.svg"
+
 export default function BottomNavigation() {
   const router = useRouter()
 
   const aClassNames = href => {
-    return classnames(
-      "flex-1 inset-m border-top-s border-color-strong",
-      { selected: router.pathname === href
-    })
+    return classnames("full-height flex justify-center align-center", { selected: router.pathname === href })
   }
 
   return (
-    <nav className="sticky-bottom backdrop-blur">
+    <nav className="sticky-bottom background-deep-0 border-top-s border-color-strong">
       <ol className="flex">
-        <li className="flex flex-1 text-center">
+        <li className="flex-1 full-height">
           <Link href="/" passHref>
             <a className={aClassNames("/")}>
-              Global
+              <WorldwideIcon className="icon" />
             </a>
           </Link>
         </li>
-        <li className="flex flex-1 text-center ">
+        <li className="flex-1 full-height">
           <Link href="/countries" passHref>
             <a className={aClassNames("/countries")}>
-              Countries
+              <ListIcon className="icon" />
+            </a>
+          </Link>
+        </li>
+        <li className="flex-1 full-height">
+          <Link href="/about" passHref>
+            <a className={aClassNames("/about")}>
+              <InfoIcon className="icon" />
             </a>
           </Link>
         </li>
